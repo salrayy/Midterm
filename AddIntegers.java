3a66b0e4e428c2c94b82603d441de4eea6041205
Summer Alrayyashi <salrayy@umich.edu>
2021-03-09 09:47:55 -0800

    Changed Integers

--- AddIntegers.java
+++ AddIntegers.java
@@ -2,8 +2,8 @@ public class AddIntegers {
 
     public static void main(String[] args) {
 
-        int first = 15;
-        int second = 45;
+        int first = 23;
+        int second = 13;
 
         System.out.println("Enter two numbers: " + first + " " + second);
         int sum = first + second;
